30aa96d97ad5b46b9856fc477ffd4ba04e57e7f3
SaniolJR <mateusz.sadowski04@wp.pl>
2025-06-14 23:08:04 +0200

    header naprawa

--- frontend/src/header.js
+++ frontend/src/header.js
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './header-css.css';
 
-// Przykładowo: userName jako props lub stała (na razie null)
-const userName = null; // lub np. "Mateusz"
-
 const Header = () => {
+  const [userNick, setUserNick] = useState(null);
+
+  useEffect(() => {
+    // Pobierz nick użytkownika jeśli jest zalogowany
+    fetch("http://localhost:5016/me", { credentials: "include" })
+      .then(res => res.ok ? res.json() : null)
+      .then(data => {
+        if (data && data.nick) setUserNick(data.nick);
+        else setUserNick(null);
+      })
+      .catch(() => setUserNick(null));
+  }, []);
+
   return (
     <nav className='header'>
       <ul>
         <li><Link to="/carts">Koszyki</Link></li>
-        {userName === null
+        {userNick === null
           ? <li><Link to="/login">Zaloguj</Link></li>
-          : <li><Link to="/account">{userName}</Link></li>
+          : <li><Link to="/account">{userNick}</Link></li>
         }
       </ul>
     </nav>
